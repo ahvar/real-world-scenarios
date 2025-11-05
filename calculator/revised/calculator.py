84367975dd678051fa78a3a33a58c9aa63afaf7d
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2025-11-05 14:23:01 -0600

    programming challenge

--- calculator/revised/calculator.py
+++ calculator/revised/calculator.py
@@ -1,53 +1,76 @@
 import csv
 import json
 
-plans = []
-zips = []
-slcsp = []
-with open("../data/plans.csv", "r") as plans_file:
-    reader = csv.DictReader(plans_file)
-    for row in reader:
-        plans.append(
-            {
-                "plan_id": row["plan_id"],
-                "state": row["state"],
-                "metal_level": row["metal_level"],
-                "rate": row["rate"],
-                "rate_area": row["rate_area"],
-            }
-        )
 
-with open("../data/zips.csv", "r") as zips_file:
-    reader = csv.DictReader(zips_file)
-    for row in reader:
-        zips.append(
-            {
-                "zipcode": row["zipcode"],
-                "state": row["state"],
-                "county_code": row["county_code"],
-                "name": row["name"],
-                "rate_area": row["rate_area"],
-            }
-        )
-with open("../data/slcsp.csv", "r") as slcsp_file:
-    zipcode = slcsp_file.readline().strip()
-    slcsp.append({"zipcode": zipcode})
-
-seen = []
-overlap = []
-for zip_data in zips:
-    if zip_data["zipcode"] in seen:
-        overlap.append(zip_data["zipcode"])
-
-for zip_data in slcsp:
-    if zip_data["zipcode"] in overlap:
-        zip_data["slcsp_rate"] = ""
+def get_rate_area_by_zip():
+    zips = {}
+    with open("../data/zips.csv", "r") as zips_file:
+        reader = csv.reader(zips_file)
+        headers = next(reader)
+        for row in reader:
+            code, state, county, name, rate_area = row
+            if code in zips:
+                zips[code] = ""
+            else:
+                zips[code] = {
+                    "rate_area": rate_area,
+                    "county": county,
+                    "name": name,
+                    "state": state,
+                }
+    return zips, headers
 
 
-"""
- slcsp_rate_areas = []
-zip_count = 0
-for slcsp_zip_code in slcsp:
-    zips = [ zip_count += 1 for zip_data in zips if slcsp_zip_code['zipcode'] == zip_data['zipcode'] ]
- 
-"""
+def find_second_lowest_silver_plans(merged):
+    slcsp = {}
+    with open("../data/plans.csv", "r") as plans_file:
+        reader = csv.reader(plans_file)
+        plans_header = next(reader)
+        for row in reader:
+            plan_id, state, metal_level, rate, rate_area = row
+            if metal_level == "Silver":
+                if rate_area in slcsp:
+                    slcsp[rate_area].append(rate)
+                else:
+                    slcsp[rate_area] = [rate]
+    second_lowest = {}
+    for area, rates in slcsp.items():
+        rates.sort()
+        if len(rates) > 1:
+            rates.sort()
+            second_lowest[area] = rates[1]
+        else:
+            second_lowest[area] = rates[0]
+
+    for zip, zipdata in merged.items():
+        for area, cost in second_lowest.items():
+            if area == zipdata["rate_area"]:
+                zipdata["second_lowest"] = cost
+                print(zipdata)
+    # print(json.dumps(merged, indent=4))
+    # print(json.dumps(second_lowest, indent=4))
+
+
+def get_slcsp_zips():
+    slcsp = {}
+    with open("../data/slcsp.csv", "r") as slcsp_file:
+        reader = csv.DictReader(slcsp_file)
+        slcsp_header = next(reader)
+        for row in reader:
+            zipcode = row["zipcode"]
+            slcsp[zipcode] = ""
+    return slcsp, slcsp_header
+
+
+if __name__ == "__main__":
+    zips_with_rates, zip_headers = get_rate_area_by_zip()
+    slcsp_zips, slcsp_header = get_slcsp_zips()
+    # print("Rate Areas by Zip Code")
+    # print(json.dumps(zips_with_rates, indent=4))
+    # print("SLCSP Zip Codes")
+    # print(json.dumps(slcsp_zips, indent=4))
+
+    merged = {**slcsp_zips, **zips_with_rates}
+    find_second_lowest_silver_plans(merged)
+    # read all plan data
+    # add the second lowest rate silver plan to the merged dataset
