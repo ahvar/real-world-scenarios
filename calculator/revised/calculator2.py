46d929163516d059222eed4ca8a4d664be591ffd
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2026-01-04 22:34:01 -0600

    slcsp calculator and customer + user data operations

--- calculator/revised/calculator2.py
+++ calculator/revised/calculator2.py
@@ -1,16 +1,14 @@
 from collections import defaultdict
 from pathlib import Path
 
-import pytest
-
-plans_filepath = Path(__file__).resolve().parent.parent / "data" / "plans.csv"
-slcsp_filepath = Path(__file__).resolve().parent.parent / "data" / "slcsp.csv"
-zips_filepath = Path(__file__).resolve().parent.parent / "data" / "zips.csv"
+plans_path = (Path(__file__).parent.parent / "data" / "plans.csv").resolve()
+slcsp_path = (Path(__file__).parent.parent / "data" / "slcsp.csv").resolve()
+zipcode_path = (Path(__file__).parent.parent / "data" / "zips.csv").resolve()
 
 
-def get_rates_by_rate_area():
+def parse_plans():
     rates_by_rate_area = defaultdict(list)
-    with open(plans_filepath, "r") as plans_in:
+    with open(plans_path, "r") as plans_in:
         plans_header = next(plans_in)
         for line in plans_in:
             plan_id, state, metal_level, rate, rate_area = line.strip().split(",")
@@ -19,56 +17,58 @@ def get_rates_by_rate_area():
     return rates_by_rate_area
 
 
-def get_slcsp_zips():
-    slcsp_zips = []
-    with open(slcsp_filepath, "r") as slcsp_in:
-        slcsp_headers = next(slcsp_in)
+def parse_slcsp_zips():
+    slcsp_zipcodes = []
+    with open(slcsp_path, "r") as slcsp_in:
+        slcsp_header = next(slcsp_in)
         for line in slcsp_in:
-            zipcode = line.strip().split(",")[0]
-            slcsp_zips.append(zipcode)
-    return slcsp_zips
+            slcsp_zipcode = line.strip().split(",")[0]
+            slcsp_zipcodes.append(slcsp_zipcode)
+    return slcsp_zipcodes
 
 
-def get_zips():
-    zips_by_rate_area = defaultdict(set)
-    with open(zips_filepath, "r") as zips_in:
+def parse_zipcodes():
+    rate_areas_by_zipcode = defaultdict(set)
+    with open(zipcode_path, "r") as zips_in:
         zips_header = next(zips_in)
         for line in zips_in:
             zipcode, state, cc, name, rate_area = line.strip().split(",")
-            zips_by_rate_area[zipcode].add((state, rate_area))
+            rate_areas_by_zipcode[zipcode].add((state, rate_area))
+    return rate_areas_by_zipcode
 
 
 if __name__ == "__main__":
+    rates_by_rate_area = parse_plans()
+    slcsp_zips = parse_slcsp_zips()
+    rate_areas_by_zipcode = parse_zipcodes()
     output = []
-    slcsp_zipcodes = get_slcsp_zips()
-    all_rates = get_rates_by_rate_area()
-    zips_by_rate_area = get_zips()
-    for zipcode in slcsp_zipcodes:
-        rate_areas = zips_by_rate_area.get(zipcode, set())
+    for zip in slcsp_zips:
+        # use the zipcode to get the state and rate_area
+        rate_areas = rate_areas_by_zipcode.get(zip, set())
         if len(rate_areas) != 1:
-            output.append((zipcode, ""))
+            output.append((zip, ""))
             continue
         state, rate_area = next(iter(rate_areas))
-        rates = sorted(set(all_rates.get((state, rate_area), [])))
+        rates = sorted(set(rates_by_rate_area.get((state, rate_area), [])))
         if len(rates) < 2:
-            output.append((zipcode, ""))
+            output.append((zip, ""))
         else:
-            output.append((zipcode, f"{rates[1]:.2f}"))
-    import sys
-
-    print("zipcode, rate")
-    for zipcode, rate in output:
-        print(f"{zipcode}, {rate}")
+            output.append((zip, f"{rates[1]:.2f}"))
 
 
 class TestSLCSPCalculator:
 
-    def test_get_rates_by_rate_area(self):
-        rates_by_rate_area = get_rates_by_rate_area()
+    def test_parse_plans(self):
+        rates_by_rate_area = parse_plans()
         assert ("KS", "9") in rates_by_rate_area
-        assert ("KY", "4") in rates_by_rate_area
+        assert ("MO", "3") in rates_by_rate_area
+        assert ("FL", "12") in rates_by_rate_area
 
-    def test_get_slcsp_zips(self):
-        slcsp_zips = get_slcsp_zips()
-        for code in ("64148", "67118", "40813"):
-            assert code in slcsp_zips
+    def test_parse_slcsp_zips(self):
+        slcsp_zips = parse_slcsp_zips()
+        for zip in ("64148", "40813", "41101"):
+            assert zip in slcsp_zips
+
+    def test_parse_zipcodes(self):
+        rate_areas_by_zipcode = parse_zipcodes()
+        assert rate_areas_by_zipcode.get("64148") == ("MO", "3")
