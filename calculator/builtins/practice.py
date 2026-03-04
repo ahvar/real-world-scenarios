21ce36c863b33ada3e35157ebba475a542316797
Arthur Vargas <arthurvargas@Arthurs-MacBook-Air.local>
2026-03-03 21:28:07 -0600

    manage hostypes and id allocation to form hostnames

--- calculator/builtins/practice.py
+++ calculator/builtins/practice.py
@@ -1,88 +1,80 @@
-import json
-import pprint
-from pathlib import Path
 from collections import defaultdict
-
-pp = pprint.PrettyPrinter(indent=4)
-
-
-def get_rates_by_rate_area():
-    rates_by_rate_area = defaultdict(list)
-    plans_path = Path(__file__).parent.parent / "data" / "plans.csv"
-    with open(plans_path, "r") as plans_in:
-        header = next(plans_in)
-        for line in plans_in:
-            plan_id, state, metal_level, rate, rate_area = line.strip().split(",")
-            if metal_level.lower() == "silver":
-                rates_by_rate_area[(state, rate_area)].append(float(rate))
-    return rates_by_rate_area
+from pathlib import Path
 
 
-def get_slcsp_zipcodes():
-    slcsp_zipcodes = []
-    slcsp_zipcode_path = Path(__file__).parent.parent / "data" / "slcsp.csv"
-    with open(slcsp_zipcode_path, "r") as slcsp_in:
-        header = next(slcsp_in)
-        for line in slcsp_in:
-            zipcode = line.strip().split(",")[0]
-            slcsp_zipcodes.append(zipcode)
-    return slcsp_zipcodes
+class SLCSPCalculator:
+
+    def __init__(self):
+        self._plans_path = Path(__file__).parent.parent / "data" / "plans.csv"
+        self._slcsp_zip = Path(__file__).parent.parent / "data" / "slcsp.csv"
+        self._zips_path = Path(__file__).parent.parent / "data" / "zips.csv"
+        self._plan_rate_by_rate_area = defaultdict(list)
+        self._slcsp_zipcodes = []
+        self._rate_area_by_zipcode = defaultdict(set)
+        self._output = []
+
+    def read_plans(self):
+        with open(self._plans_path, "r") as plans_in:
+            header = next(plans_in)
+            for line in plans_in:
+                plan_id, state, metal_level, rate, rate_area = line.strip().split(",")
+                if metal_level.lower() == "silver":
+                    self._plan_rate_by_rate_area[(state, rate_area)] = float(rate)
+
+    def read_slcsp_zip(self):
+        with open(self._slcsp_zip, "r") as slcsp_zip_in:
+            header = next(slcsp_zip_in)
+            for line in slcsp_zip_in:
+                zipcode = line.strip().split(",")[0]
+                self._slcsp_zipcodes.append(zipcode)
+
+    def read_zips(self):
+        with open(self._zips_path, "r") as zips_in:
+            header = next(zips_in)
+            for line in zips_in:
+                zipcode, state, cc, name, rate_area = line.strip().split(",")
+                self._rate_area_by_zipcode[zipcode].add((state, rate_area))
+
+    def calculate_slcsp(self):
+        for zipcode in self._slcsp_zipcodes:
+            rate_area = self._rate_area_by_zipcode.get(zipcode, set())
+            if len(rate_area) != 1:
+                self._output.append((zipcode, ""))
+                continue
+            state, rate_area = next(iter(rate_area))
+            rates = sorted(self._plan_rate_by_rate_area[(state, rate_area)])
+            if len(rates) < 2:
+                self._output.append((zipcode, ""))
+            else:
+                self._output.append((zipcode, f"{rates[1]:.2f}"))
+
+    @property
+    def plans(self):
+        return self._plan_rate_by_rate_area
+
+    @property
+    def slcsp_zip(self):
+        return self._slcsp_zipcodes
+
+    @property
+    def zipcodes(self):
+        return self._rate_area_by_zipcode
 
 
-def get_rate_area_zips():
-    rate_area_by_zipcode = defaultdict(set)
-    rate_areas_path = Path(__file__).parent.parent / "data" / "zips.csv"
-    with open(rate_areas_path, "r") as ra_in:
-        header = next(ra_in)
-        for line in ra_in:
-            zipcode, state, cc, name, rate_area = line.strip().split(",")
-            rate_area_by_zipcode[zipcode].add((state, rate_area))
-    return rate_area_by_zipcode
+class TestSlcspCalculator:
 
+    def setup_method(self):
+        self.slcsp_calculator = SLCSPCalculator()
 
-def calculate_slcsp_premium():
-    rates_by_rate_area = get_rates_by_rate_area()
-    slcsp_zipcodes = get_slcsp_zipcodes()
-    rate_area_by_zipcode = get_rate_area_zips()
-    output = []
-    for zipcode in slcsp_zipcodes:
-        rate_areas = rate_area_by_zipcode.get(zipcode, set())
-        if len(rate_areas) != 1:
-            output.append[(zipcode, "")]
-            continue
-        state, rate_area = next(iter(rate_areas))
-        rates = sorted(set(rates_by_rate_area[(state, rate_area), []]))
-        if len(rates) < 2:
-            output.append((zipcode, ""))
-        else:
-            output.append((zipcode, f"{rates[1]:.2f}"))
+    def test_read_plans(self):
+        self.slcsp_calculator.read_plans()
+        assert ("MO", "3") in self.slcsp_calculator.plans
 
-    for (
-        zipcode,
-        rate,
-    ) in output:
-        print(f"{zipcode}, {rate}")
+    def test_read_slcsp_zip(self):
+        self.slcsp_calculator.read_slcsp_zip()
+        assert "64148" in self.slcsp_calculator.slcsp_zip
 
-
-if __name__ == "__main__":
-    calculate_slcsp_premium()
-
-
-class TestSLCSPCalculator:
-
-    def test_get_rates_by_rate_area(self):
-        rates_by_rate_area = get_rates_by_rate_area()
-        pp.pprint(rates_by_rate_area)
-        for rate_area in [("MO", "3"), ("KY", "4"), ("PA", "7")]:
-            assert rate_area in rates_by_rate_area
-
-    def test_get_slcsp_zipcodes(self):
-        slcsp_zipcodes = get_slcsp_zipcodes()
-        pp.pprint(slcsp_zipcodes)
-        for zip in ["64148", "40813", "51012"]:
-            assert zip in slcsp_zipcodes
-
-    def test_get_rate_area_zips(self):
-        rate_area_zips = get_rate_area_zips()
-        pp.pprint(rate_area_zips)
-        assert len(rate_area_zips["41101"]) == 1
+    def test_read_zips(self):
+        self.slcsp_calculator.read_zips()
+        assert "64148" in self.slcsp_calculator.zipcodes
+        assert self.slcsp_calculator.zipcodes["64148"] == {("MO", "3")}
--- server_name_tracker/scratch.py
+++ server_name_tracker/scratch.py
@@ -1,28 +1,79 @@
-import heapq
-
-hosttypes = {}
-
-
-def bucket():
-    return {"next_id": 1, "free_ids": [], "allocated": set()}
+from collections import defaultdict
+import heapq, re
 
 
-def allocate(hosttype):
-    server_name = ""
-    # if free_ids has something, use the smallest free_id
-    if hosttypes[hosttype]["free_ids"]:
-        id = heapq.heappop(hosttypes[hosttype]["free_ids"])
-    else:
-        id = hosttypes[hosttype]["next_id"]
-        hosttypes[hosttype]["allocated"].add(id)
-        hosttypes[hosttype]["next_id"] += 1
+class Tracker:
+
+    def __init__(self):
+        self.hosttypes = {}
+
+    def valid_name(self, hosttype) -> bool:
+        pass
+
+    def allocate(self, hosttype) -> str:
+        # no servers being tracked for this hosttype
+        if hosttype not in self.hosttypes:
+            # make a new entry in the hosttypes dict and an object for managing ids
+            self.hosttypes[hosttype] = {
+                "next_id": 1,
+                "free_ids": [],
+                "allocated": set(),
+            }
+            # get the next id; 1 in this case because there are no servers of this hosttype
+            id = self.hosttypes[hosttype].get("next_id")
+            # add the id to the set of allocated ids for this hosttype
+            self.hosttypes[hosttype]["allocated"].add(id)
+            # bump the next_id count
+            self.hosttypes[hosttype]["next_id"] += 1
+            # return the server name
+            return f"{hosttype}{id}"
+
+        # there are already servers for this hosttype and there are some ids available to use
+        # for this new allocation operation
+        if self.hosttypes[hosttype]["free_ids"]:
+            # pop the next id off the heap
+            id = heapq.heappop(self.hosttypes[hosttype]["free_ids"])
+            # add it to the allocated set
+            self.hosttypes[hosttype]["allocated"].add(id)
+            # return the server name
+            return f"{hosttype}{id}"
+        if not self.hosttypes[hosttype]["free_ids"]:
+            # get the next id
+            id = self.hosttypes[hosttype].get("next_id")
+            # bump the next id
+            self.hosttypes[hosttype]["next_id"] += 1
+            # add it to the allocated set
+            self.hosttypes[hosttype]["allocated"].add(id)
+            return f"{hosttype}{id}"
+
+    def parse_server_name(self, hostname):
+        for c in hostname:
+            if c.isdigit():
+                return hostname[: hostname.index(c)], int(hostname[hostname.index(c) :])
+
+    def parse_server_name_regex(self, servername):
+        m = re.match(f"^([a-zA-Z_-]+)(\d+)$", servername)
+        if m:
+            return m.group(1), int(m.group(2))
+        return None, None
+
+    def deallocate(self, hostname):
+        # parse hosttype and id
+        hosttype, id = self.parse_server_name(hostname)
+        if hosttype not in self.hosttypes:
+            print(f"error: {hosttype} not allocated")
+            return
+        # remove id from allocated
+        self.hosttypes[hosttype]["allocated"].remove(id)
+        # push to the heap; Olog(n) time ?
+        heapq.heappush(self.hosttypes[hosttype]["free_ids"], id)
 
 
-def deallocate(hosttype):
-    pass
+class TestTracker:
+    def setup_method(self):
+        self.tracker = Tracker()
 
-
-names = [("api", allocate), ("web", deallocate), ("db", deallocate), ("api", allocate)]
-
-for server, operation in names:
-    operation(server)
+    def test_allocate(self):
+        assert self.tracker.allocate("api") == "api1"
+        assert self.tracker.allocate("api") == "api2"
+        assert self.tracker.allocate("api") == "api3"
